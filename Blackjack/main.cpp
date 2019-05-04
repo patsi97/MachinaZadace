168cf85bc7c257765bbf08eb1053c777e1e0a9a5
patsi97 <48284682+patsi97@users.noreply.github.com>
2019-05-04 18:20:25 +0200

    izmjena koda

--- Blackjack/main.cpp
+++ Blackjack/main.cpp
@@ -13,16 +13,49 @@ public:
 	std::string _name;
 	int _sum;
 
-};
-int generateCard() {
-	int min = 1, max = 10, generatedCard = 0;
-	generatedCard = rand() % (max - min + 1) + min;
-	return generatedCard;
-}
+	int generateCard() {
+		int min = 1, max = 10, generatedCard = 0;
+		generatedCard = rand() % (max - min + 1) + min;
+		return generatedCard;
+	}
 
-int addCard(int sum, int generatedCard) {
-	sum += generatedCard;
-	return sum;
+	int addCard() {
+		_sum += generateCard();
+		return _sum;
+	}
+};
+std::string whoIsTheWinner(Player playerOne, Player computer)
+{
+	std::cout << "Computer sum:" << computer._sum << std::endl;
+	std::cout << "Your sum:" << playerOne._sum << std::endl;
+
+	if (playerOne._sum <= 21 && computer._sum <= 21) {
+		if (playerOne._sum > computer._sum) {
+			return playerOne._name + " is a winner!";
+			//std::cout << playerOne._name << " is a winner!" << std::endl;
+		}
+		else if (playerOne._sum < computer._sum)
+		{
+			return computer._name + "is a winner!";
+			//std::cout << computer._name << " is a winner!" << std::endl;
+		}
+		else {
+			return "draw!";
+			//std::cout << "draw!" << std::endl;
+		}
+	}
+
+	else {
+		if (playerOne._sum > 21) {
+			return computer._name + " is a winner!";
+			//std::cout << computer._name << " is a winner!" << std::endl;
+		}
+		else if (computer._sum > 21)
+		{
+			return playerOne._name + "is a winner!";
+			//std::cout << playerOne._name << " is a winner!" << std::endl;
+		}
+	}
 }
 
 int main() {
@@ -30,57 +63,48 @@ int main() {
 	srand(time(NULL));
 	Player playerOne = Player("Korisnik", 0);
 	Player computer = Player("Computer", 0);
-	
+
 	std::cout << "Welcome to blackjack!" << std::endl;
 	std::cout << "Chose y/n to take a card: ";
+	computer.addCard();
 
-	while (playerOne._sum < 21 && computer._sum < 21) {
-		
+	while (playerOne._sum < 21) {
+
 		std::cin >> pickCard;
 
 		if (pickCard == "y") {
-			playerOne._sum = addCard(playerOne._sum, generateCard());
-			std::cout << "Your sum:" << playerOne._sum << std::endl;
+			playerOne.addCard();
 			
-		}
-
-		else if (pickCard == "n") {
-			
-			computer._sum = addCard(computer._sum, generateCard());
-			std::cout << "Computer sum:" << computer._sum << std::endl;
-			std::cout << "Your sum:" << playerOne._sum << std::endl;
-
-			if ((computer._sum > playerOne._sum && computer._sum >= 13 && computer._sum <= 21)) {
+			if (playerOne._sum > 21)
+			{
 				break;
 			}
-			
 		}
 
+		else if (pickCard == "n") {			
+			break;
+
+		}
+		std::cout << "Computer sum:" << computer._sum << std::endl;
+		std::cout << "Your sum:" << playerOne._sum << std::endl;
 	}
+	while (playerOne._sum < 21 && computer._sum < 21) {
+		if (computer._sum < 13 || computer._sum<playerOne._sum ) {
+			computer.addCard();
 
-	if (playerOne._sum <= 21 && computer._sum <= 21) {
-		if (playerOne._sum > computer._sum) {
-			std::cout << playerOne._name << " is a winner!" << std::endl;
 		}
-		else if(playerOne._sum < computer._sum)
+		else 
 		{
-			std::cout << computer._name << " is a winner!" << std::endl;
+			break;
 		}
-		else {
-			std::cout <<  "draw!" << std::endl;
-		}
+
+		std::cout << "Computer sum:" << computer._sum << std::endl;
+		std::cout << "Your sum:" << playerOne._sum << std::endl;
+
 	}
 
-	else {
-		if (playerOne._sum > 21) {
-			std::cout << computer._name << " is a winner!" << std::endl;
-		}
-		else if (computer._sum > 21)
-		{
-			std::cout << playerOne._name << " is a winner!" << std::endl;
-		}
-	}
-	
+	std::cout << whoIsTheWinner(playerOne, computer) << std::endl;
+
 	std::cin.get();
 	system("pause");
 }
